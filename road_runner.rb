e10ad2217cc69bedd5fbd7154338aa21306bf38b
snavinch <snavinch@visa.com>
2019-09-28 20:12:13 +0530

    + Added condition to handle null/empty response

--- road_runner.rb
+++ road_runner.rb
@@ -57,10 +57,10 @@ Class.new Test::Unit::TestCase do
 				# define a variable to determine if sample code can be called.
 				call_sample_code = true
 				
-				if unique_name.include? "Token_Management"
-				    input_fields.push("93B32398-AD51-4CC2-A682-EA3E93614EB1")
-				elsif unique_name.include? "GetReportDefinition"
-				    input_fields.push("TransactionRequestClass")
+				if class_qualified_name.include? "Token_Management"
+			    		input_fields.push("93B32398-AD51-4CC2-A682-EA3E93614EB1")
+				elsif class_qualified_name.include? "GetReportDefinition"
+			    		input_fields.push("TransactionRequestClass")
 				end
 
 				# Check if the required fields are in the global map
@@ -75,8 +75,8 @@ Class.new Test::Unit::TestCase do
 				end
 				
 				if !dependent_sample_code.empty?
-					if unique_name.include? "Retrieve" or unique_name.include? "Delete"
-		    				sleep 15
+					if class_qualified_name.include? "Retrieve" or class_qualified_name.include? "Delete"
+		    				sleep 20
 					end
 				end
 
@@ -96,37 +96,45 @@ Class.new Test::Unit::TestCase do
 						response, http_status = instance.run()
 					end
 
-					# Convert the Json response to hash
-					hash_response = JSON.parse(response)
+					if !response.nil? and !response.empty?
+						# Convert the Json response to hash
+						hash_response = JSON.parse(response)
 
-					# Store the response values into global map which will be used for subsequent requests
-					for field in stored_fields
-						value = find(hash_response,field)
-						unless value.nil?
-							global_map.store(unique_name + field, value)
+						# Store the response values into global map which will be used for subsequent requests
+						for field in stored_fields
+							value = find(hash_response,field)
+							unless value.nil?
+								global_map.store(unique_name + field, value)
+							end
 						end
-					end
 
-					# Start assertions
+						# Start assertions
 
-					unless road["Assertions"]["httpStatus"].empty?
-						assert_equal(road["Assertions"]["httpStatus"], (http_status.nil?) ? http_status : http_status.to_s, "Actual value of \"httpStatus\" field doesn't match Expected value in the response.")
-					end
+						unless road["Assertions"]["httpStatus"].nil? or road["Assertions"]["httpStatus"].empty?
+							assert_equal(road["Assertions"]["httpStatus"], (http_status.nil?) ? http_status : http_status.to_s, "Actual value of \"httpStatus\" field doesn't match Expected value in the response.")
+						end
+					
+						unless road["Assertions"]["requiredFields"].nil?
+							for required_field in road["Assertions"]["requiredFields"]
+								actual_value = find(hash_response,required_field)
+								assert_equal(true, !actual_value.nil?, required_field + " - is a required field, but not present in the response.")
+							end
+						end
 
-					for required_field in road["Assertions"]["requiredFields"]
-						actual_value = find(hash_response,required_field)
-						assert_equal(true, !actual_value.nil?, required_field + " - is a required field, but not present in the response.")
-					end
-
-					for expected_value in road["Assertions"]["expectedValues"]
-						actual_value = find(hash_response,expected_value["field"])
-						assert_equal(expected_value["value"], actual_value, "Actual value of \"" + expected_value["field"] + "\" field doesn't match Expected value in the response.")
+						unless road["Assertions"]["expectedValues"].nil?
+							for expected_value in road["Assertions"]["expectedValues"]
+								actual_value = find(hash_response,expected_value["field"])
+								assert_equal(expected_value["value"], actual_value, "Actual value of \"" + expected_value["field"] + "\" field doesn't match Expected value in the response.")
+							end
+						end
+					else
+						assert_equal(true, false, "Sample code execution failed as response returned is null/empty")
 					end
 				end
 
 			rescue StandardError => err
 				# in case of a standard exception
-				assert_equal(true, false, "Sample code execution/comparison failed due to an exception - " + err.message)
+				assert_equal(true, false, "Sample code wasn't executed as an exception occured in road runner - " + err.message)
 
 			rescue LoadError => err
 				# in case of a load error exception, where the sample code is not found/cannot be loaded
@@ -135,3 +143,4 @@ Class.new Test::Unit::TestCase do
 		end
 	end
 end
+
