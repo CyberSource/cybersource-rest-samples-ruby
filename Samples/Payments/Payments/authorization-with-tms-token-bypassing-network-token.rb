5bc9e07ef4c1a01c7947df5136b34b09ca48c930
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2021-04-01 19:17:09 +0530

    Upgrade to latest version

---
+++ Samples/Payments/Payments/authorization-with-tms-token-bypassing-network-token.rb
@@ -0,0 +1,45 @@
+require 'cybersource_rest_client'
+require_relative '../../../data/Configuration.rb'
+
+public
+class Authorization_with_tms_token_bypassing_network_token
+    def run()
+        request_obj = CyberSource::CreatePaymentRequest.new
+        client_reference_information = CyberSource::Ptsv2paymentsClientReferenceInformation.new
+        client_reference_information.code = "TC50171_3"
+        request_obj.client_reference_information = client_reference_information
+
+        payment_information = CyberSource::Ptsv2paymentsPaymentInformation.new
+        instrument_identifier = CyberSource::Ptsv2paymentsPaymentInformationInstrumentIdentifier.new
+        instrument_identifier.id = "7010000000016241111"
+        payment_information.instrument_identifier = instrument_identifier
+        request_obj.payment_information = payment_information
+
+        order_information = CyberSource::Ptsv2paymentsOrderInformation.new
+        amount_details = CyberSource::Ptsv2paymentsOrderInformationAmountDetails.new
+        amount_details.total_amount = "102.21"
+        amount_details.currency = "USD"
+        order_information.amount_details = amount_details
+        request_obj.order_information = order_information
+
+        token_information = CyberSource::Ptsv2paymentsTokenInformation.new
+        token_information.network_token_option = "ignore"
+        request_obj.token_information = token_information
+
+        config = MerchantConfiguration.new.merchantConfigProp()
+        api_client = CyberSource::ApiClient.new
+        api_instance = CyberSource::PaymentsApi.new(api_client, config)
+
+        data, status_code, headers = api_instance.create_payment(request_obj)
+
+        puts data, status_code, headers
+
+        return data
+    rescue StandardError => err
+        puts err.message
+    end
+    if __FILE__ == $0
+
+        Authorization_with_tms_token_bypassing_network_token.new.run()
+    end
+end
