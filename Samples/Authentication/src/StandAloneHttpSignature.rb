14950a10ca5b6af5ebd9feb064c5f6641976fff6
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2021-05-05 17:56:13 +0530

    Updated to new version of SDK with removed hostnames

--- Samples/Authentication/src/StandAloneHttpSignature.rb
+++ Samples/Authentication/src/StandAloneHttpSignature.rb
@@ -1,7 +1,7 @@
 require 'digest'
 require 'openssl'
 require 'date'
-require 'uri'
+require 'addressable/uri'
 require 'base64'
 require 'json'
 require 'net/http'
@@ -61,21 +61,21 @@ class StandAloneHttpSignature
     signatureHeader = ''
     signatureHeaderValue << "keyid=\"" + @@merchant_key_id + "\""
     signatureHeaderValue << ", algorithm=\"HmacSHA256\""
-    
+
     if http_method == "post"
         signatureHeader = 'host date (request-target) digest v-c-merchant-id'
     elsif http_method == "get"
         signatureHeader = 'host date (request-target) v-c-merchant-id'
     end
-    
+
     signatureHeaderValue << ", headers=\"" + signatureHeader + "\""
-    
+
     signatureString = 'host: ' + @@request_host
     signatureString << "\ndate: " + gmtdatetime
     signatureString << "\n(request-target): "
-    
+
     targetUrl = http_method + ' ' + resource
-    
+
     signatureString << targetUrl
 
     if http_method == "post"
@@ -84,17 +84,17 @@ class StandAloneHttpSignature
         digest_payload = 'SHA-256=' + digest
         signatureString << "\ndigest: " + digest_payload
     end
-    
+
     signatureString << "\nv-c-merchant-id: " + @@merchant_id
-    
+
     encodedSignatureString = signatureString.force_encoding(Encoding::UTF_8)
     decodedKey = Base64.decode64(@@merchant_secret_key)
     base64EncodedSignature = Base64.strict_encode64(OpenSSL::HMAC.digest('sha256', decodedKey, encodedSignatureString))
-    
+
     signature_value = base64EncodedSignature
-    
+
     signatureHeaderValue << ", signature=\"" + signature_value + "\""
-    
+
     return signatureHeaderValue
   end
   
@@ -102,22 +102,22 @@ class StandAloneHttpSignature
     resource = "/pts/v2/payments/"
     method = "post"
     statusCode = -1
-    url = URI.encode("https://" + @@request_host + resource)
-    
+    url = Addressable::URI.encode("https://" + @@request_host + resource)
+
     header_params = {}
     header_params['Accept'] = 'application/hal+json;charset=utf-8'
     header_params['Content-Type'] = 'application/json;charset=utf-8'
-    
+
     auth_names = []
     gmtDateTime = DateTime.now.httpdate
-    
+
     token = getHttpSignature(resource, method, gmtDateTime)
-    
+
     header_params['v-c-merchant-id'] = @@merchant_id
     header_params['Date'] = gmtDateTime
     header_params['Host'] = @@request_host
     header_params['Signature'] = token
-    
+
     payload = @@payload
     digest = Digest::SHA256.base64digest(payload)
     digest_payload = 'SHA-256=' + digest
@@ -134,8 +134,19 @@ class StandAloneHttpSignature
     puts "\tHost : " + @@request_host + "\n"
 	puts "\tSignature : " + token + "\n"
 	puts "\tDigest : " + digest + "\n"
-    
-    uri = URI(url)
+
+    uri = Addressable::URI.parse(url)
+    uri.port = 443
+
+    # IMPORTANT NOTE:
+    # Addressable does not set the port unless explicitly supplied in the URL.
+    # According to the developer of Addressable:
+        # Yeah, this was one of those tough design decisions where I opted to break strict compatibility with the URI class because I think
+        # it's doing the wrong thing. The way the standard library's implementation was designed, you can't easily tell if someone has
+        # overridden the default port without parsing the authority component manually yourself (which URI doesn't actually give you access
+        # to); so that's super-lame. The first version of Addressable (back in the 0.0.1 days) tried to be 100% compatible here and the
+        # issue came up so often that I eventually decided that URI's decision was just wrong and opted for the opposite one instead.
+        # Addressable is a parser, not a magical 'try-to-figure-out-what-you-want-in-this-context' library.
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -146,18 +157,18 @@ class StandAloneHttpSignature
     end
 
     response = http.request(req)
-    
+
     if response.code.to_i >= 200 && response.code.to_i <= 299
         statusCode = 0
     end
-    
+
     puts "\n -- Response Message -- \n"
     puts "\tResponse Code : " + response.code + "\n"
     puts "\tv-c-correlation-id : " + response['v-c-correlation-id'] + "\n"
     puts "\n"
     puts "\tResponse Data :\n"
     puts response.body + "\n\n"
-    
+
     return statusCode;
   end
   
@@ -165,26 +176,26 @@ class StandAloneHttpSignature
     resource = "/reporting/v3/reports?startTime=2021-01-01T00:00:00.0Z&endTime=2021-01-02T23:59:59.0Z&timeQueryType=executedTime&reportMimeType=application/xml"
     method = "get"
     statusCode = -1
-    url = URI.encode("https://" + @@request_host + resource)
-    
+    url = Addressable::URI.encode("https://" + @@request_host + resource)
+
     header_params = {}
     header_params['Accept'] = 'application/hal+json;charset=utf-8'
     header_params['Content-Type'] = 'application/json;charset=utf-8'
-    
+
     auth_names = []
     gmtDateTime = DateTime.now.httpdate
-    
+
     token = getHttpSignature(resource, method, gmtDateTime)
-    
+
     header_params['v-c-merchant-id'] = @@merchant_id
     header_params['Accept-Encoding'] = '*'
     header_params['Date'] = gmtDateTime
     header_params['Host'] = @@request_host
     header_params['User-Agent'] = "Mozilla/5.0"
     header_params['Signature'] = token
-    
+
     headers = @@default_headers.merge(header_params || {})
-    
+
     puts "\n -- RequestURL -- \n"
     puts "\tURL : " + url + "\n"
     puts "\n -- HTTP Headers -- \n"
@@ -192,14 +203,25 @@ class StandAloneHttpSignature
     puts "\tv-c-merchant-id : " + @@merchant_id + "\n"
     puts "\tDate : " + gmtDateTime + "\n"
     puts "\tHost : " + @@request_host + "\n"
-	puts "\tSignature : " + token + "\n"
-    
-    uri = URI(url)
+    puts "\tSignature : " + token + "\n"
+
+    uri = Addressable::URI.parse(url)
+    uri.port = 443
+
+    # IMPORTANT NOTE:
+    # Addressable does not set the port unless explicitly supplied in the URL.
+    # According to the developer of Addressable:
+        # Yeah, this was one of those tough design decisions where I opted to break strict compatibility with the URI class because I think
+        # it's doing the wrong thing. The way the standard library's implementation was designed, you can't easily tell if someone has
+        # overridden the default port without parsing the authority component manually yourself (which URI doesn't actually give you access
+        # to); so that's super-lame. The first version of Addressable (back in the 0.0.1 days) tried to be 100% compatible here and the
+        # issue came up so often that I eventually decided that URI's decision was just wrong and opted for the opposite one instead.
+        # Addressable is a parser, not a magical 'try-to-figure-out-what-you-want-in-this-context' library.
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    req = Net::HTTP::Get.new(uri)
-    
+    req = Net::HTTP::Get.new("#{uri.path}?#{uri.query}")
+
     header_params.each do |custom_header, custom_header_value|
         req[custom_header] = custom_header_value
     end
@@ -209,7 +231,7 @@ class StandAloneHttpSignature
     if response.code.to_i >= 200 && response.code.to_i <= 299
         statusCode = 0
     end
-    
+
     puts "\n -- Response Message -- \n"
     puts "\tResponse Code : " + response.code + "\n"
     puts "\tv-c-correlation-id : " + response['v-c-correlation-id'] + "\n"
@@ -217,7 +239,7 @@ class StandAloneHttpSignature
     puts "\n"
     puts "\tResponse Data :\n"
     puts response.body + "\n\n"
-    
+
     return statusCode;
   end
   
@@ -225,23 +247,23 @@ class StandAloneHttpSignature
     # HTTP POST REQUEST
     puts "\n\nSample 1: POST call - CyberSource Payments API - HTTP POST Payment request"
     @statusCode = processPost()
-    
+
     if @statusCode == 0
         puts "STATUS : SUCCESS (HTTP Status = #@statusCode)"
     else
         puts "STATUS : ERROR (HTTP Status = #@statusCode)"
     end
-        
+    
     # HTTP GET REQUEST
     puts "\n\nSample 2: GET call - CyberSource Reporting API - HTTP GET Reporting request"
     @statusCode = processGet()
-    
+
     if @statusCode == 0
         puts "STATUS : SUCCESS (HTTP Status = #@statusCode)"
     else
         puts "STATUS : ERROR (HTTP Status = #@statusCode)"
     end
-    
+
   end
   
   if __FILE__ == $0
