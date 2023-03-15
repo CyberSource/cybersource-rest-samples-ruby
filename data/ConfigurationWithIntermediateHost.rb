f12c86b553600a10d5b26a02359888f4fe1a38ee
sachshet <sachshet@visa.com>
2023-03-15 10:32:49 +0530

    axa sample updates

---
+++ data/ConfigurationWithIntermediateHost.rb
@@ -0,0 +1,130 @@
+require 'cybersource_rest_client'
+
+public
+class MerchantConfiguration
+  def merchantConfigProp()
+    # Common Paramaters
+    merchantId='testrest'
+    runEnvironment='apitest.cybersource.com'
+    intermediateHost='http://localhost:8080'
+    timeout=1000 #In Milliseconds
+    authenticationType='http_signature'
+    jsonFilePath='resource/request.json'
+    enableLog=true
+    loggingLevel='DEBUG'
+    logDirectory='log'
+    logFilename='cybs'
+    maxLogSize=10485760
+    maxLogFiles=5
+    enableMasking=true
+    # proxyAddress='userproxy.com'
+    # proxyPort=443
+    # HTTP Parameters
+    merchantKeyId='08c94330-f618-42a3-b09d-e1e43be5efda'
+    merchantSecretKey='yBJxy6LjM2TmcPGu+GaJrHtkke25fPpUX+UY6/L/1tE='
+    # JWT Parameters
+    keysDirectory='resource'
+    keyAlias='testrest'
+    keyPass='testrest'
+    keyFilename='testrest'
+
+    # MetaKey Params
+    useMetaKey = false
+    portfolioID = ''
+    
+    configurationDictionary={}
+    configurationDictionary['merchantID'] = merchantId
+    configurationDictionary['runEnvironment'] = runEnvironment
+    configurationDictionary['intermediateHost'] = intermediateHost
+    configurationDictionary['timeout'] = timeout
+    configurationDictionary['authenticationType'] = authenticationType
+    configurationDictionary['jsonFilePath'] = jsonFilePath
+    # configurationDictionary['proxyPort'] = proxyPort
+    # configurationDictionary['proxyAddress'] = proxyAddress
+    configurationDictionary['merchantsecretKey'] = merchantSecretKey
+    configurationDictionary['merchantKeyId'] = merchantKeyId
+    configurationDictionary['keysDirectory'] = keysDirectory
+    configurationDictionary['keyAlias'] = keyAlias
+    configurationDictionary['keyPass'] = keyPass
+    configurationDictionary['useMetaKey'] = useMetaKey
+    configurationDictionary['portfolioID'] = portfolioID
+    configurationDictionary['keyFilename'] = keyFilename
+
+    log_config = {}
+    log_config['enableLog'] = enableLog
+    log_config['loggingLevel'] = loggingLevel
+    log_config['logDirectory'] = logDirectory
+    log_config['logFilename'] = logFilename
+    log_config['maxLogSize'] = maxLogSize
+    log_config['maxLogFiles'] = maxLogFiles
+    log_config['enableMasking'] = enableMasking
+
+    configurationDictionary['logConfiguration'] = log_config
+
+    custom_headers = {}
+    custom_headers['Host'] = 'localhost'
+    custom_headers['Ocp-Apim-Subscription-Key'] = 'Test555555'
+
+    configurationDictionary['defaultCustomHeaders'] = custom_headers
+    return configurationDictionary
+  end
+
+  def alternativeMerchantConfigProp()
+    # Common Paramaters
+    merchantId='testrest_cpctv'
+    runEnvironment='apitest.cybersource.com'
+    timeout=1000 #In Milliseconds
+    authenticationType='http_signature'
+    jsonFilePath='resource/request.json'
+    enableLog=true
+    loggingLevel='DEBUG'
+    logDirectory='log'
+    logFilename='cybs'
+    maxLogSize=10485760
+    maxLogFiles=5
+    enableMasking=true
+    # proxyAddress='userproxy.com'
+    # proxyPort=443
+    # HTTP Parameters
+    merchantKeyId='e547c3d3-16e4-444c-9313-2a08784b906a'
+    merchantSecretKey='JXm4dqKYIxWofM1TIbtYY9HuYo7Cg1HPHxn29f6waRo='
+    # JWT Parameters
+    keysDirectory='resource'
+    keyAlias='testrest_cpctv'
+    keyPass='testrest_cpctv'
+    keyFilename='testrest_cpctv'
+
+    # MetaKey Params
+    useMetaKey = false
+    portfolioID = ''
+    
+    configurationDictionary={}
+    configurationDictionary['merchantID'] = merchantId
+    configurationDictionary['runEnvironment'] = runEnvironment
+    configurationDictionary['timeout'] = timeout
+    configurationDictionary['authenticationType'] = authenticationType
+    # configurationDictionary['jsonFilePath'] = jsonFilePath
+    # configurationDictionary['proxyPort'] = proxyPort
+    # configurationDictionary['proxyAddress'] = proxyAddress
+    configurationDictionary['merchantsecretKey'] = merchantSecretKey
+    configurationDictionary['merchantKeyId'] = merchantKeyId
+    configurationDictionary['keysDirectory'] = keysDirectory
+    configurationDictionary['keyAlias'] = keyAlias
+    configurationDictionary['keyPass'] = keyPass
+    configurationDictionary['useMetaKey'] = useMetaKey
+    configurationDictionary['portfolioID'] = portfolioID
+    configurationDictionary['keyFilename'] = keyFilename
+
+    log_config = {}
+    log_config['enableLog'] = enableLog
+    log_config['loggingLevel'] = loggingLevel
+    log_config['logDirectory'] = logDirectory
+    log_config['logFilename'] = logFilename
+    log_config['maxLogSize'] = maxLogSize
+    log_config['maxLogFiles'] = maxLogFiles
+    log_config['enableMasking'] = enableMasking
+
+    configurationDictionary['logConfiguration'] = log_config
+    return configurationDictionary
+  end
+end
