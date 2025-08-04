e0e2f7259f66e197fe9d556eef1e25032eea79e1
Aastvik Goel <aastgoel@visa.com>
2025-08-04 10:55:47 +0530

    net http library test

--- Samples/Authentication/src/StandAloneJWT.rb
+++ Samples/Authentication/src/StandAloneJWT.rb
@@ -4,6 +4,7 @@ require 'jwt'
 require 'json'
 require 'date'
 require 'net/http'
+require 'net/http/persistent'
 require 'addressable/uri'
 require 'active_support'
 require 'time'
@@ -53,7 +54,20 @@ class StandAloneJWT
         "  }" +
         "}"
 
+  # HTTP Persistent connection configuration
+  @@keep_alive_timeout = 1 # seconds
+  @@connection_pool_size = 5
+  # Initialize persistent HTTP client
+  @@http_persistent = Net::HTTP::Persistent.new(name: 'cybersource_api')
+
   @@default_headers = {}
+
+  def initialize
+    # Configure persistent connection settings
+    @@http_persistent.idle_timeout = @@keep_alive_timeout
+    # @@http_persistent.pool_size = @@connection_pool_size
+    @@http_persistent.verify_mode = OpenSSL::SSL::VERIFY_PEER
+  end
 
   # Function to get the Json Web Token
   # param: resource - denotes the resource being accessed
@@ -100,6 +114,7 @@ class StandAloneJWT
     method = "post"
     statusCode = -1
     url = Addressable::URI.encode("https://" + @@request_host + resource)
+    uri = Addressable::URI.parse(url)
 
     header_params = {}
     header_params['Accept'] = 'application/hal+json;charset=utf-8'
@@ -108,13 +123,13 @@ class StandAloneJWT
     auth_names = []
     gmtDateTime = DateTime.now.httpdate
 
-    puts "\n -- RequestURL -- \n"
-    puts "\tURL : " + url + "\n"
-    puts "\n -- HTTP Headers -- \n"
-    puts "\tContent-Type : application/json;charset=utf-8" + "\n"
-    puts "\tv-c-merchant-id : " + @@merchant_id + "\n"
-    puts "\tDate : " + gmtDateTime + "\n"
-    puts "\tHost : " + @@request_host + "\n"
+    # puts "\n -- RequestURL -- \n"
+    # puts "\tURL : " + url + "\n"
+    # puts "\n -- HTTP Headers -- \n"
+    # puts "\tContent-Type : application/json;charset=utf-8" + "\n"
+    # puts "\tv-c-merchant-id : " + @@merchant_id + "\n"
+    # puts "\tDate : " + gmtDateTime + "\n"
+    # puts "\tHost : " + @@request_host + "\n"
 
     token = "Bearer " + getJsonWebToken(resource, method, gmtDateTime)
 
@@ -131,39 +146,39 @@ class StandAloneJWT
 
     headers = @@default_headers.merge(header_params || {})
 
-    uri = Addressable::URI.parse(url)
-    uri.port = 443
+    # Create a POST request for persistent HTTP client
+    5.times do |i|
+      sleep(6)
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req.body = @@payload
+      header_params.each do |custom_header, custom_header_value|
+          req[custom_header] = custom_header_value
+      end
 
-    # IMPORTANT NOTE:
-    # Addressable does not set the port unless explicitly supplied in the URL.
-    # According to the developer of Addressable:
-        # Yeah, this was one of those tough design decisions where I opted to break strict compatibility with the URI class because I think
-        # it's doing the wrong thing. The way the standard library's implementation was designed, you can't easily tell if someone has
-        # overridden the default port without parsing the authority component manually yourself (which URI doesn't actually give you access
-        # to); so that's super-lame. The first version of Addressable (back in the 0.0.1 days) tried to be 100% compatible here and the
-        # issue came up so often that I eventually decided that URI's decision was just wrong and opted for the opposite one instead.
-        # Addressable is a parser, not a magical 'try-to-figure-out-what-you-want-in-this-context' library.
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    req = Net::HTTP::Post.new(uri.path)
-    req.body = @@payload
-    header_params.each do |custom_header, custom_header_value|
-        req[custom_header] = custom_header_value
+      # Execute the request with persistent connection
+      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+      response = @@http_persistent.request(uri, req)
+      end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+      time_taken = (end_time - start_time) / 1_000_000 # ms
+      if response.code.to_i >= 200 && response.code.to_i <= 299
+          statusCode = 0
+      else
+          statusCode = -1
+      end
+      if statusCode == 0
+        puts "Call #{i+1} STATUS : SUCCESS took #{time_taken} to complete."
+      else
+        puts "Call #{i+1} STATUS : ERROR"
+      end
     end
 
-    response = http.request(req)
 
-    if response.code.to_i >= 200 && response.code.to_i <= 299
-        statusCode = 0
-    end
-
-    puts "\n -- Response Message -- \n"
-    puts "\tResponse Code : " + response.code + "\n"
-    puts "\tv-c-correlation-id : " + response['v-c-correlation-id'] + "\n"
-    puts "\n"
-    puts "\tResponse Data :\n"
-    puts response.body + "\n\n"
+    # puts "\n -- Response Message -- \n"
+    # puts "\tResponse Code : " + response.code + "\n"
+    # puts "\tv-c-correlation-id : " + response['v-c-correlation-id'] + "\n"
+    # puts "\n"
+    # puts "\tResponse Data :\n"
+    # puts response.body + "\n\n"
 
     return statusCode;
   end
@@ -173,6 +188,7 @@ class StandAloneJWT
     method = "get"
     statusCode = -1
     url = Addressable::URI.encode("https://" + @@request_host + resource)
+    uri = Addressable::URI.parse(url)
 
     header_params = {}
     header_params['Accept'] = 'application/hal+json;charset=utf-8'
@@ -201,28 +217,16 @@ class StandAloneJWT
 
     headers = @@default_headers.merge(header_params || {})
 
-    uri = Addressable::URI.parse(url)
-    uri.port = 443
-
-    # IMPORTANT NOTE:
-    # Addressable does not set the port unless explicitly supplied in the URL.
-    # According to the developer of Addressable:
-        # Yeah, this was one of those tough design decisions where I opted to break strict compatibility with the URI class because I think
-        # it's doing the wrong thing. The way the standard library's implementation was designed, you can't easily tell if someone has
-        # overridden the default port without parsing the authority component manually yourself (which URI doesn't actually give you access
-        # to); so that's super-lame. The first version of Addressable (back in the 0.0.1 days) tried to be 100% compatible here and the
-        # issue came up so often that I eventually decided that URI's decision was just wrong and opted for the opposite one instead.
-        # Addressable is a parser, not a magical 'try-to-figure-out-what-you-want-in-this-context' library.
-
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    req = Net::HTTP::Get.new("#{uri.path}?#{uri.query}")
-
+    # Create a GET request for persistent HTTP client
+    request_uri = uri.request_uri
+    req = Net::HTTP::Get.new(request_uri)
+    
     header_params.each do |custom_header, custom_header_value|
         req[custom_header] = custom_header_value
     end
 
-    response = http.request(req)
+    # Execute the request with persistent connection
+    response = @@http_persistent.request(uri, req)
 
     if response.code.to_i >= 200 && response.code.to_i <= 299
         statusCode = 0
@@ -237,6 +241,11 @@ class StandAloneJWT
     puts response.body + "\n\n"
 
     return statusCode;
+  end
+
+  def cleanup
+    # Close the persistent connection when done
+    @@http_persistent.shutdown
   end
 
   def write_log_audit(status)
@@ -257,21 +266,24 @@ class StandAloneJWT
     end
 
     # HTTP GET REQUEST
-    puts "\n\nSample 2: GET call - CyberSource Reporting API - HTTP GET Reporting request"
-    @statusCode = processGet()
-    statusCodeGet = @statusCode
+    # puts "\n\nSample 2: GET call - CyberSource Reporting API - HTTP GET Reporting request"
+    # @statusCode = processGet()
+    # statusCodeGet = @statusCode
 
-    if @statusCode == 0
-        puts "STATUS : SUCCESS (HTTP Status = #@statusCode)"
-    else
-        puts "STATUS : ERROR (HTTP Status = #@statusCode)"
-    end
+    # if @statusCode == 0
+    #     puts "STATUS : SUCCESS (HTTP Status = #@statusCode)"
+    # else
+    #     puts "STATUS : ERROR (HTTP Status = #@statusCode)"
+    # end
 
-    if statusCodePost == 0 and statusCodeGet == 0
-        write_log_audit(200)
-    else
-        write_log_audit(400)
-    end
+    # if statusCodePost == 0 and statusCodeGet == 0
+    #     write_log_audit(200)
+    # else
+    #     write_log_audit(400)
+    # end
+    
+    # Clean up connections when done
+    cleanup
   end
 
   if __FILE__ == $0
